27d53b797f27622eada406310bcd8ed94cc00dbe
Arkadiusz Torun <arek.torun12@gmail.com>
2024-02-11 22:37:02 +0100

    removed unnecessary gaps

--- script.js
+++ script.js
@@ -42,17 +42,16 @@
 
         for (const task of tasks) {
             tasksListHtmlContent += `
-            <li class="tasks__item js-task"
-            >
-            <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-            ${task.done ? "🗸" : ""}
-            </button>
-            <span class="tasks__content ${ task.done ? "tasks__content--done" : ""}">
-            ${task.content}
-            </span>
-            <button class="tasks__button tasks__button--remove js-remove">🗑️</button>
+            <li class="tasks__item js-task">
+                <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+                    ${task.done ? "🗸" : ""}
+                </button>
+                <span class="tasks__content ${ task.done ? "tasks__content--done" : ""}">
+                    ${task.content}
+                </span>
+                <button class="tasks__button tasks__button--remove js-remove">🗑️</button>
             </li>
-            `;
+        `;
         }
 
         document.querySelector(".js-tasks").innerHTML = tasksListHtmlContent;
