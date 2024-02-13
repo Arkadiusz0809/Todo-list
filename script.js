85412b0f77e6c551189c71c291112b296dcd1711
Arkadiusz Torun <arek.torun12@gmail.com>
2024-02-13 19:38:14 +0100

    added immutablity + 2 extra buttons

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 {
-    const tasks = [];
+    let tasks = [
+        { content: "buy butter",
+        done: false,
+    }
+    ];
+    let hideDoneTasks = false;
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({ content: newTaskContent});
-            render();
+        tasks = [
+            ...tasks,
+            { content: newTaskContent }
+        ]
+        render();
     };
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1)
+        ],
+            render();
+    };
+
+    const markAllTasksDone = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }));
+
         render();
     };
+
+    const toggleHideDoneTasks = () => {
+        hideDoneTasks = !hideDoneTasks;
+        render();
+    }
 
     bindRemoveEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
@@ -36,28 +68,67 @@
         });
     };
 
-
-    const render = () => {
-        let tasksListHtmlContent = "";
-
-        for (const task of tasks) {
-            tasksListHtmlContent += `
-            <li class="tasks__item js-task">
+    const renderTasks = () => {
+        const taskToHTML = task => `
+            <li class=
+            "tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""} js-task
+            ">
                 <button class="tasks__button tasks__button--toggleDone js-toggleDone">
                     ${task.done ? "🗸" : ""}
                 </button>
-                <span class="tasks__content ${ task.done ? "tasks__content--done" : ""}">
+                <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
                     ${task.content}
                 </span>
                 <button class="tasks__button tasks__button--remove js-remove">🗑️</button>
             </li>
         `;
+
+        const tasksElement = document.querySelector(".js-tasks");
+        tasksElement.innerHTML = tasks.map(taskToHTML).join("");
+    };
+
+    const renderButtons = () => {
+        const buttonsElement = document.querySelector(".js-buttons");
+
+        if (!tasks.length) {
+            buttonsElement.innerHTML = "";
+            return;
+        }
+        buttonsElement.innerHTML = `
+            <h2 class="task__header">Lista zadań</h2>
+                <button class="js-toggleHideDoneTasks task__buttonHide">
+                    ${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button class="task__buttonComplete js-markAllDone"
+                    ${tasks.every(({ done }) => done) ? " disabled " : ""}>
+                    Ukończ wszystkie
+                </button>
+            `;
+    };
+
+    const bindButtonsEvents = () => {
+
+        const toggleHideDoneTasksButton = document.querySelector("js-toggleHideDoneTasks");
+        const markAllDoneButton = document.querySelector("js-markAllDone");
+
+        if (markAllDoneButton) {
+            markAllDoneButton.addEventListener("click", markAllTasksDone);
         }
 
-        document.querySelector(".js-tasks").innerHTML = tasksListHtmlContent;
+        if (toggleHideDoneTasksButton) {
+            toggleHideDoneTasksButton.addEventListener("click", toggleHideDoneTasks);
+        }
+    };
 
+
+    const render = () => {
+        renderTasks();
         bindRemoveEvents();
         bindToggleDoneEvents();
+
+        renderButtons();
+        bindButtonsEvents();
+
     };
 
 
